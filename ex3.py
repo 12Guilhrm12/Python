6c080915dcaeebed17a92c4f12a2c5152745a5e3
Justin <12guilhrm12@gmail.com>
2025-08-15 10:04:38 -0300

    Delete ex3.py

--- ex3.py
+++
@@ -1,9 +0,0 @@
-try:
-    a = float(input('Digite o multiplicando: ')); 
-    b = float(input('Digite o multiplicador: ')); 
-    result = a * b; 
-    print(f'Resultado: {result}'); 
-except ValueError:
-    print('Digite apenas números inteiros.')
-finally:
-    print('Operação realizada com sucesso.')

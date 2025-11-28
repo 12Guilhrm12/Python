ab2d557070c94c893eabf839d6cf0a5112b47ec6
Justin <12guilhrm12@gmail.com>
2025-11-28 10:45:52 -0300

    Correios

---
+++ Correios/main.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from tkinter import ttk
+
+from tkinter import messagebox
+
+class Menu(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        
+        self.title = ttk.Label(self, text='Correios', background='yellow', foreground='blue', font=('Busso', 36, 'bold'))
+        self.title.grid(row=0, column=0, padx=(250, 0), pady=(110, 0))
+
+        # Login Section
+
+        self.user_text = ttk.Label(self, text='User: ')
+        self.user_text.grid(row=2, column=0, pady=(50, 0), padx=(100, 0))
+
+        self.login_entry = ttk.Entry(self)
+        self.login_entry.grid(row=2, column=0, pady=(50, 0), padx=(325, 0))
+
+        self.pass_text = ttk.Label(self, text='Password: ')
+        self.pass_text.grid(row=3, column=0, pady=(12, 0), padx=(100, 0))
+
+        self.pass_entry = ttk.Entry(self)
+        self.pass_entry.grid(row=3, column=0, pady=(12, 0), padx=(325, 0))
+
+        self.button = ttk.Button(self, text='Login', command=self.login)
+        self.button.grid(row=4, column=0, pady=(12, 0), padx=(312, 0))
+
+        self.grid()
+    
+    def login(self):
+        password = self.pass_entry.get()
+        user = self.login_entry.get()
+
+        if (password == 'correios12') and (user == 'Justin'):
+            messagebox.showinfo(title='Correios', message='Sejá bem-vindo!')
+            self.frame = Entregas(app)
+            
+        else:
+            messagebox.showerror(title='Erro!', message='Usúario ou Senha incorretos!')
+
+class Entregas(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        
+        self.title = ttk.Label(self, text='Correios', background='yellow', foreground='blue', font=('Busso', 36, 'bold'))
+        self.title.grid(row=0, column=0, padx=(250, 0), pady=(110, 0))
+
+        self.grid()
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Correios')
+        self.geometry('720x460')
+
+
+if __name__ == "__main__":
+    app = App(); 
+    frame = Menu(app); 
+    app.mainloop(); 
